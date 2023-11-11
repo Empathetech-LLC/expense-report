2237a82b067ed8d91c3cef55dc664d946f63a4d6
empathictech <105748722+empathictech@users.noreply.github.com>
2023-11-10 19:13:19 -0500

    updated file names

--- expense-report.py
+++ expense-report.py
@@ -2,15 +2,16 @@ import pandas as pd
 import sys
 import os
 
-def process_csv(input_file_path):
-    directory = os.path.dirname(input_file_path)
+def process_csv(expense_data_path):
+    directory, base_name = os.path.split(expense_data_path)
+    file_date = base_name.split('.')[0]
 
     # Set the output file paths
-    output_modified_path = os.path.join(directory, 'modified_file.csv')
-    output_ytd_totals_path = os.path.join(directory, 'ytd_totals_file.csv')
+    clean_output_path = os.path.join(directory, f'{file_date}-cleaned.csv')
+    ytd_totals_output_path = os.path.join(directory, f'{file_date}-ytd-totals.csv')
 
     # Read the CSV file, skipping the first row
-    df = pd.read_csv(input_file_path, skiprows=1)
+    df = pd.read_csv(expense_data_path, skiprows=1)
 
     # Remove unnecessary columns
     df.drop(['No.', 'Payee', 'Memo'], axis=1, inplace=True)
@@ -21,19 +22,19 @@ def process_csv(input_file_path):
     # Filter out rows where Type is 'Credit Card Payment'
     df_filtered = df[df['Type'] != 'Credit Card Payment']
 
-    # Write the modified data to a new CSV file
-    df_filtered.to_csv(output_modified_path, index=False)
+    # Write the cleaned data to a new CSV file
+    df_filtered.to_csv(clean_output_path, index=False)
 
     # Calculate YTD totals for each category
     ytd_totals = df_filtered.groupby('Category')['Total'].sum().reset_index()
 
     # Write YTD totals to another CSV file
-    ytd_totals.to_csv(output_ytd_totals_path, index=False)
+    ytd_totals.to_csv(ytd_totals_output_path, index=False)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <input_file_path>")
+        print("Usage: python script.py <file_path>")
         sys.exit(1)
 
-    input_file_path = sys.argv[1]
-    process_csv(input_file_path)
+    expense_data_path = sys.argv[1]
+    process_csv(expense_data_path)
